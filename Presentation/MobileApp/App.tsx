15060cecdf39e5d90bbf37c89dd7d73b01da11ea
Nguyễn Bá Minh Vũ <mvu22k412345@gmail.com>
2025-10-12 22:09:53 +0700

    Update App.tsx

--- Presentation/MobileApp/App.tsx
+++ Presentation/MobileApp/App.tsx
@@ -4,17 +4,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./src/Auth/Login";
 import SignUp from "./src/Auth/SignUp";
+import ForgotPassword from "./src/Auth/ForgotPassword"; // mới thêm
+import ChangePassword from "./src/Auth/ChangePassword"; // mới thêm vào 
 import Home from "./src/Home/Home";
 import Doanchat from "./src/Messegers/Doanchat";
 import Messenger from "./src/Messegers/Messenger";
-const Stack = createStackNavigator();
-export default function App() {
 
+const Stack = createStackNavigator();
+
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" >
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false}} />
+        <Stack.Screen name="Forgot" component={ForgotPassword} options={{ headerShown: false}} />
+        <Stack.Screen name="ChangePassword" component={ChangePassword} options={{ headerShown: false}} />
         <Stack.Screen name="Home" component={Home} options={{ headerShown: false}}/>
         <Stack.Screen name="Messenger" component={Messenger} options={{ headerShown: false}}/>
         <Stack.Screen name="Doanchat" component={Doanchat} options={{ headerShown: false}}/>
