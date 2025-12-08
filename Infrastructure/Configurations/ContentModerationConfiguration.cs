6b4958bce3af7d5279a26d060ef4265c7df5bd58
Hoang <hoangzai2k401@gmail.com>
2025-12-08 19:12:12 +0700

    Cập nhật cấu hình kết nối và API base URL

--- Infrastructure/Configurations/ContentModerationConfiguration.cs
+++ Infrastructure/Configurations/ContentModerationConfiguration.cs
@@ -18,16 +18,16 @@ namespace UngDungMangXaHoi.Infrastructure.Configurations
             builder.Property(cm => cm.ModerationID).HasColumnName("ModerationID").ValueGeneratedOnAdd();
             builder.Property(cm => cm.ContentType).HasMaxLength(20).IsRequired();
             builder.Property(cm => cm.ContentID).IsRequired();
-            builder.Property(cm => cm.accountId).HasColumnName("account_id").IsRequired();
-            builder.Property(cm => cm.postId).HasColumnName("post_id");
-            builder.Property(cm => cm.commentId).HasColumnName("comment_id");
+            builder.Property(cm => cm.AccountId).HasColumnName("account_id").IsRequired();
+            builder.Property(cm => cm.PostId).HasColumnName("post_id");
+            builder.Property(cm => cm.CommentId).HasColumnName("comment_id");
             builder.Property(cm => cm.AIConfidence).IsRequired();
             builder.Property(cm => cm.ToxicLabel).HasMaxLength(50).IsRequired();
             builder.Property(cm => cm.Status).HasMaxLength(20);
             builder.Property(cm => cm.CreatedAt).HasColumnType("datetime").HasDefaultValueSql("getdate()");
-            builder.HasOne(p => p.Post).WithMany(cm => cm.ContentModerations).HasForeignKey(c => c.postId).OnDelete(DeleteBehavior.SetNull);
-            builder.HasOne(com => com.Comment).WithMany(cm => cm.ContentModerations).HasForeignKey(c => c.commentId).OnDelete(DeleteBehavior.SetNull);
-            builder.HasOne(a => a.Account).WithMany(cm => cm.ContentModerations).HasForeignKey(c => c.accountId).OnDelete(DeleteBehavior.Cascade);
+            builder.HasOne(p => p.Post).WithMany(cm => cm.ContentModerations).HasForeignKey(c => c.PostId).OnDelete(DeleteBehavior.SetNull);
+            builder.HasOne(com => com.Comment).WithMany(cm => cm.ContentModerations).HasForeignKey(c => c.CommentId).OnDelete(DeleteBehavior.SetNull);
+            builder.HasOne(a => a.Account).WithMany(cm => cm.ContentModerations).HasForeignKey(c => c.AccountId).OnDelete(DeleteBehavior.Cascade);
 
         }
     }
